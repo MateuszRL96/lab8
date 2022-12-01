61db83bf7a98a214555bb8fe5eccaf6d717c8e48
Mateusz  Kulec <w64875@student.wsiz.edu.pl>
2022-12-01 13:07:20 +0100

    new file

---
+++ MVC_LAB/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using MVC_LAB.Models.Person;
+using SQLitePCL;
+
+namespace MVC_LAB.Services
+{
+    public interface IPersonService
+    {
+        public List<PersonModel> GetPersons();
+
+        public void CreatePerson(int id, string name, string city, GenderEnum gender);
+        
+    }
+}
